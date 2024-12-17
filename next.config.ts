0ab104e138cb7a433587c6e3bfbc20f9b7c6820d
iamsven2005 <iamsven2005@gmail.com>
2024-12-17 11:55:46 +0800

    initial-comit

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+export default {
+  images: {
+    dangerouslyAllowSVG: true,
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        pathname: '/s/files/**'
+      }
+    ]
+  }
+};
